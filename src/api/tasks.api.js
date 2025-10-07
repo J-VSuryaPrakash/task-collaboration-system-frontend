d63c38f1f8f540ec8cae07d8d263978ca18cf36c
Surya Prakash <surya.jinagam@gmail.com>
2025-10-07 23:36:09 +0530

    added task api

---
+++ src/api/tasks.api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = import.meta.env.VITE_BASE_URL
+
+export const fetchTasks = async (projectId) => {
+  try {
+    const res = await axios.get(`${BASE_URL}/tasks/fetch-all-tasks`, {
+      params: { projectId },
+      withCredentials: true,
+    });
+    return res.data;
+  } catch (error) {
+    console.error("Error fetching tasks:", error);
+    throw error;
+  }
+};
