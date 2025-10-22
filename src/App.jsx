34b7b96d6d1bb7ed73d58c750186005cd60d240a
Surya Prakash <surya.jinagam@gmail.com>
2025-10-22 17:43:31 +0530

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './index.css'
 import LandingPage from './pages/LandingPage.jsx'
-import SignupPage from './pages/SignUpPage.jsx'
+import SignupPage from './pages/SignupPage.jsx'
 import LoginPage from './pages/LoginPage.jsx'
 import DashBoard from './pages/DashBoard.jsx'
 import NavBar from './components/NavBar.jsx'
@@ -12,16 +12,6 @@ function App() {
 
   return (
     <>
-      {/* <div className='flex justify-center items-center h-screen'>
-        <h1 className='text-3xl font-bold bg-gray-900 text-white'>Hello React JS</h1>
-      </div> */}
-      {/* <LandingPage /> */}
-      {/* <SignupPage /> */}
-      {/* <LoginPage /> */}
-      {/* <DashBoard /> */}
-      {/* <NavBar/> */}
-      {/* <BoardPage /> */}
-      {/* <TaskCard /> */}
     </>
   )
 }
